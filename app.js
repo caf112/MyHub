d4af90ffb3976bd7a78ba24bb2d92bfd9577b9c6
caf112 <yuu10san.14@gmail.com>
2024-09-10 20:13:24 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '0361',
+    database: 'myhub_db'
+});
+
+app.get('/',(req,res) => {
+    res.render('top.ejs');
+});
+
+app.get('/index',(req,res) => {
+    connection.query(
+        'SELECT * FROM items',
+        (error,results) => {
+            res.render('index.ejs',{items: results});
+        }
+    );
+});
+
+app.get('/new',(req,res) => {
+    res.render('create.ejs');
+});
+
+app.post('/create',(req,res) => {
+    connection.query(
+        'INSERT INTO items (title) VALUES (?)',
+        [req.body.newTitle],
+        (error,results) => {
+            res.redirect('/index');
+        }
+    );
+});
+
+app.listen(3000);
